f493220accea07dce04a77fd69107b9607a934b6
sabeom <tkqja717@naver.com>
2024-06-07 14:53:36 +0900

    Initial commit

---
+++ src/main/java/com/ict_final/issue_trend/IssueTrendApplication.java
@@ -0,0 +1,13 @@
+package com.ict_final.issue_trend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ict_final/issue_trend/IssueTrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict_final.issue_trend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
